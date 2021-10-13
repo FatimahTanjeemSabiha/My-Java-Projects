8ee520f2162a69f945105f014803bdfac1f0e0df
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2021-10-13 15:46:15 +0600

    Add files via upload

---
+++ ReversingANumber/src/FatimahTanjeemSabiha/Reverse.java
@@ -0,0 +1,20 @@
+package FatimahTanjeemSabiha;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+        int a,res=0,n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        n=sc.nextInt();
+        while(n!=0)
+        {
+            a=n%10;
+            res=(res*10)+a;
+            n=n/10;
+        }
+        System.out.println("Reverse of the number is: "+res);
+    }
+}
